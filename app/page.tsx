5f3112b74893d8ba579fb701abd11737e6975e68
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-06 16:30:02 +0300

    I completed  my shadcn ui and add a default height of 100%

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className='text-3xl font-bold text-sky-500'>Hello World!</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-sky-500'>Hello World!</h1>
+      <Button variant="default">Click Me</Button>
+    </div>
+  );
 }
