2d5ecd41048a79dc74f5ea68396b693cb9353b0e
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-10 17:47:26 +0300

    I imported the sidebar component in my dashboard to my layout file

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,9 @@
+const SideBar = () => {
+  return (
+    <div className='h-full border-r flex-col overflow-y-auto bg-white shadow-sm'>
+      sidebar!
+    </div>
+  );
+};
+
+export default SideBar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,14 @@
+import Sidebar from "./_components/sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className='h-full'>
+      <div className='hidden md:flex z-50 h-full w-56 flex-col bg-fixed inset-y-0'>
+        <Sidebar />
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
