eca847be5843388b72373aa46f9bcea2df02e137
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-14 21:30:50 +0300

    I added a guest route dashboard property

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,7 +1,21 @@
 'use client';
 
+import { Layout } from 'lucide-react';
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+];
 const SidebarRoutes = () => {
-  return <div className="flex flex-col w-full">sidebarroutes!</div>;
+  return <div className='flex flex-col w-full'>sidebarroutes!</div>;
 };
 
 export default SidebarRoutes;
