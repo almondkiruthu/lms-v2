f0936bf4f091339abbe0bd8d722ae289a31b37ca
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-16 12:52:38 +0300

    I created the Navbar component

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="h-full items-center border-b bg-white p-4 shadow-sm"></div>
+  );
+};
+
+export default Navbar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,16 @@
+import Navbar from "./_components/navbar";
 import Sidebar from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-full">
-      <div className="inset-y-0 z-50 hidden h-full w-56 flex-col bg-fixed md:flex">
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-50 h-[5rem] w-full md:pl-56">
+        <Navbar />
+      </div>
+      <div className="inset-y-0 z-50 hidden h-full w-56 flex-col fixed md:flex">
         <Sidebar />
       </div>
-      <main className="">{children}</main>
+      <main className="md:pl-56 h-full">{children}</main>
     </div>
   );
 };
