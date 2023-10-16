66acd86afa3429406b54ed952f453d64d2871503
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-16 12:35:36 +0300

    I added the prettier tailwind plugin and setit up with the prettier config file

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,12 @@
-import Sidebar from './_components/sidebar';
+import Sidebar from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full flex'>
-      <div className='hidden md:flex z-50 h-full w-56 flex-col bg-fixed inset-y-0'>
+    <div className="flex h-full">
+      <div className="inset-y-0 z-50 hidden h-full w-56 flex-col bg-fixed md:flex">
         <Sidebar />
       </div>
-      <main className=''>{children}</main>
+      <main className="">{children}</main>
     </div>
   );
 };
