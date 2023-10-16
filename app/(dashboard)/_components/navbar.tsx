a69a05f88e5f107c8fa038a8dfff1c2643c360e5
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-16 13:06:50 +0300

    I added the sheet component to design the mobile responsive navbar from shadcn

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,10 @@
+import MobileSidebar from "./mobile-sidebat";
+
 const Navbar = () => {
   return (
-    <div className="h-full items-center border-b bg-white p-4 shadow-sm"></div>
+    <div className="h-full items-center border-b bg-white p-4 shadow-sm">
+        <MobileSidebar />
+    </div>
   );
 };
 
