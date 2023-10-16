cb4518384994043e31861303eed330e494c093dd
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-16 12:33:35 +0300

    Edited the home layout styling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,11 +2,11 @@ import Sidebar from './_components/sidebar';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full'>
+    <div className='h-full flex'>
       <div className='hidden md:flex z-50 h-full w-56 flex-col bg-fixed inset-y-0'>
         <Sidebar />
       </div>
-      <main className='md:pl-56 h-full'>{children}</main>
+      <main className=''>{children}</main>
     </div>
   );
 };
