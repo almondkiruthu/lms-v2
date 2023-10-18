4a8eb24f16fd17cad070edf8ae811a7ec3e3819f
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-18 11:35:28 +0300

    I chnged the course id to be unique in the schema

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,10 +1,29 @@
-const CourseIdPage = ({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
   params,
 }: {
   params: {
     courseId: string;
   };
 }) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
   return <div>Course Id:{params.courseId}</div>;
 };
 
