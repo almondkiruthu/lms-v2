30322f49d8a3bc7243c421476320985d6f25b22b
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-19 10:44:29 +0300

    I changed the description toast update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -50,7 +50,7 @@ const DescriptionForm = ({ intialData, courseId }: DescriptionFormProps) => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.patch(`/api/courses/${courseId}`, values);
-      toast.success("Course title updated");
+      toast.success("Course description updated");
       toggleEdit();
       router.refresh();
     } catch (error) {
