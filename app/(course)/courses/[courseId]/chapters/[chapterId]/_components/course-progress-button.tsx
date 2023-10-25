40fd4d384ef4c5b348c53ba603e85d6b29a46671
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-25 16:34:41 +0300

    I created the progress route in the api

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -29,7 +29,9 @@ export const CourseProgressButton = ({
   const onClick = async () => {
     try {
       setIsLoading(true);
-
+      // if (setIsLoading(true)) {
+      //   toast.loading("Updating progress");
+      // }
       await axios.put(
         `/api/courses/${courseId}/chapters/${chapterId}/progress`,
         {
